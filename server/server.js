25db641aabb8df186a7e7f74ea105663db0addfe
Ross Sheppard <ross.sheppard@hpe.com>
2017-06-17 21:03:36 -0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -42,6 +42,17 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var {id} = req.params;
+
+  if(!ObjectID.isValid(id)) return res.status(404).send();
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) return res.status(404).send();
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
+})
+
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
 });
